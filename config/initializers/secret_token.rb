10963bf39ce7b162b361007f897bb986c1b71c3b
Tatiana Hegai <tatiana.hegai@iaau.edu.kg>
2013-12-15 01:43:33 +0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MathIaauEduKg::Application.config.secret_key_base = '3c902436b2a7029b74f8bd41de956fe3ab78d504070e8173262856737124fd5c0d00a2b80de66730515bc8f1e5ffa2da5b395882db507c967becafb6e8284650'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MathIaauEduKg::Application.config.session_store :cookie_store, key: '_math_iaau_edu_kg_session'
