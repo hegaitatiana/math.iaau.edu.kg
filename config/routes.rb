9751928d670b2ea73d4618e1057451e7cfc9e61f
Tatiana Hegai <tatiana.hegai@iaau.edu.kg>
2013-12-15 02:24:32 +0600

    Add precompiled assets for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 MathIaauEduKg::Application.routes.draw do
+  get "welcome/index"
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
