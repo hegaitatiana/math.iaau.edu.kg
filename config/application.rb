e99db5a3d30cc53399febaf4d7942ed8b39b7ca9
Tatiana Hegai <tatiana.hegai@iaau.edu.kg>
2013-12-18 22:46:54 +0600

    compiled assets

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(:default, Rails.env)
 
 module MathIaauEduKg
   class Application < Rails::Application
-    config.assets.enabled=true
+    config.assets.initialize_on_precompile=false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
