44d164a880ac3e94224a0d0b2c6a1b560f1f61b2
Tatiana Hegai <tatiana.hegai@iaau.edu.kg>
2013-12-18 22:56:17 +0600

    Modifiying files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ MathIaauEduKg::Application.configure do
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
-
+  config.assets.initialize_on_precompile=false
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
